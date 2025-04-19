5cf6f584402c9f2b867f36e59f05cbe9f7f9c239
Rajan Barot <ribarot03@gmail.com>
2025-04-19 02:34:08 -0700

    testing header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -722,12 +722,13 @@ export default function RootLayout({
               <div className="relative pt-[140px] pb-[50px] bg-white dark:bg-my-black border-b border-[#DCD7D7] dark:border-[#263c6b]">
                 <div className="max-w-6xl mx-auto px-3 h-full flex items-center justify-center">
                   <h1 className="font-raleway text-center leading-[48px] text-2xl md:text-[40px] text-my-black dark:text-white font-bold lg:max-w-[700px]">
-                    {" "}
+                    {/* {" "}
                     The Most Accurate SIP Calculator To
                     <span className="text-dark-green">
                       {" "}
                       Get Rich SIP by SIP{" "}
-                    </span>{" "}
+                    </span>{" "} */}
+                    TEST
                   </h1>
                 </div>
               </div>
